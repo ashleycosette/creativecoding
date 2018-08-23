365ec01a145d20e9784c3b1f9c54e0b27d4d3db1
Ashley Levine <ashleylevine@Ashleys-MacBook-Pro.local>
2018-08-23 13:41:24 -0400

    update homework

--- week1/sketch.js
+++ week1/sketch.js
@@ -7,23 +7,9 @@ function setup() {
 }
 
 function draw() {
-  // ellipse(50, 50, 80, 80);
-  // ellipse(100, 100, 20, 20);
-  // ellipse(200, 200, 20, 10);
-  // ellipse(300, 350, 160, 80);
-  // ellipse(400, 250, 500, 60);
-  // ellipse(500, 50, 120, 60);
-  // ellipse(600, 300, 120, 20);
-  ellipse(300, 200, 200, 200);
-  ellipse(300, 58, 85, 85);
-  ellipse(280, 50, 20, 20);
-  ellipse(320, 50, 20, 20);
-  ellipse(400, 126, 50, 50);
-  rect(25, 25, 50, 50);
-  rect(50, 50, 100, 100);
-  rect(75, 75, 125, 125);
-  rect(300, 300, 50, 50);
-  rect(325, 325, 40, 40);
-  rect(350, 350, 25, 25);
+  ellipse(300, 200, 170, 80);
+  ellipse(300, 210, 80, 20);
+  ellipse(340, 160, 20, 20);
+  ellipse(260, 160, 20, 20);
   // put drawing code here
 }
