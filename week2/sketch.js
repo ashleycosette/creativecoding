f44ef9fd3395e50d7f1f2f7dbd30a6dd2ab1d035
Ashley Levine <ashleylevine@Ashleys-MacBook-Pro.local>
2018-08-29 12:52:51 -0400

    week2 homework created

---
+++ week2/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800, 400);
+  background(255, 175, 252);
+  // put setup code here
+}
+
+function draw() {
+  ellipse(300, 200, 170, 80);
+  ellipse(300, 210, 80, 20);
+  ellipse(340, 160, 20, 20);
+  ellipse(260, 160, 20, 20);
+  // put drawing code here
+}
