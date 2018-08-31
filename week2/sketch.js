d8e5d7aa30aaab05c17a6c8ab01a083ae7583475
Ashley Levine <ashleylevine@Ashleys-MBP.attlocal.net>
2018-08-31 11:16:37 -0400

    week2

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,11 +1,83 @@
 function setup() {
+  // put setup code here
+
   createCanvas(800, 400);
   background(204);
-  // put setup code here
+  angleMode(DEGREES);
+  strokeWeight(10);
+  noStroke();
+
 }
 
 function draw() {
   // put drawing code here
+
+//   fill(250, 0, 255);
+//   ellipse(100, 60, 80, 40);
+// face
+//
+//   fill(0, 255, 191);
+//   ellipse(80, 40, 10, 10);
+//   ellipse(120, 40, 10, 10);
+// eyes
+//
+//   fill(0, 26, 255);
+//   ellipse(100, 65, 40, 10);
+// mouth
+//
+//   fill(255, 255, 0);
+//   triangle(80, 80, 40, 120, 120, 120);
+//   line(40, 80, 120, 80);
+// triangle and line on top
+
+
+  // ellipse(100, 100, 20, 20);
+  // rect(90, 110, 20, 50);
+  // line(50, 90, 90, 110);
+  // line(110, 110, 150, 90);
+  // line(110, 160, 150, 180);
+  // line(150, 90, 150, 180);
+  // line(50, 180, 90, 160);
+  // line(50, 90, 50, 180);
+  // butterfly
+
+  // fill(222, 255, 0);
+  // arc(90, 60, 80, 80, 45, 315);
+  // arc(190, 60, 80, 80, 45, 315);
+  // arc(290, 60, 80, 80, 45, 315);
+  // arc(390, 60, 80, 80, 45, 315);
+  // multiple pacmans
+
+  // fill(222, 255, 0);
+  // arc(90, 60, 80, 80, 45, 315);
+  // arc(190, 60, 80, 80, 45, 330);
+  // arc(290, 60, 80, 80, 45, 345);
+  // arc(390, 60, 80, 80, 45, 400);
+  // multiple pacmans different mouths
+
+  // strokeWeight(1);
+  // ellipse(90, 60, 80, 80);
+  // strokeWeight(8);
+  // ellipse(180, 60, 80, 80);
+  // strokeWeight(15);
+  // ellipse(280, 60, 80, 80);
+  // stroke weight widths
+
+  // strokeJoin(ROUND);
+  // rect(140, 25,70, 70);
+  // stroke join to make shape border round, round the stroke corners
+
+  // strokeJoin(BEVEL);
+  // rect(140, 25,70, 70);
+  // stroke join to make shape border bevel, bevel the stroke corners
+
+  // fill(153);
+  // ellipse(132, 82, 200, 200);
+  //
+  // noFill();
+  // ellipse(228, -16, 200, 200);
+  // circles that overlap and no fill
+
   fill(0);
   beginShape();
   vertex(50, 120);
@@ -20,6 +92,22 @@ function draw() {
   endShape();
   fill(255, 255, 0);
   ellipse(155, 60, 8, 8);
+  // created a creature
+
+  fill(255);
+  beginShape();
+  vertex(370, 120);
+  vertex(360, 90);
+  vertex(290, 80);
+  vertex(340, 70);
+  vertex(280, 50);
+  vertex(420, 10);
+  vertex(390, 50);
+  vertex(410, 90);
+  vertex(460, 120);
+  endShape();
+  fill(0);
+  ellipse(345, 50, 10, 10);
   // created a creature
 
 
