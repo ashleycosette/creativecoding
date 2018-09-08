a02f644990c69d7538435b3d4b88b86205bbfe68
Ashley Levine <ashleylevine@Ashleys-MBP.attlocal.net>
2018-09-08 09:15:10 -0400

    update homework 3

--- week3/sketch.js
+++ week3/sketch.js
@@ -1,13 +1,3 @@
-// var x = 240;
-// var y = 60;
-// var d = 10;
-// for circles examples
-
-// var x = 80;
-// var y = 40;
-// var d = 70;
-// var s = 20;
-
 var x = 80;
 var y = 40;
 var d = 100;
@@ -21,19 +11,8 @@ function setup() {
 }
 
 function draw() {
-  // var x = 240;
-  // var y = 60;
-  // x = 50;
-  // ellipse(x, y, d, d);
-  // ellipse(x, y, 3*d, 3*d);
-  // ellipse(x, y, 9*d, 9*d);
-// circles example
-
   rect(x, y, s, s);
   rect(x+s+d, y, 2*s, 2*s);
   rect(x+s+d+2*s+d, y, 3*s, 3*s);
-
-
-
   // put drawing code here
 }
