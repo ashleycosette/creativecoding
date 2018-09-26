7972612495d38046aaff47ac5c83189539b4e62d
Ashley Levine <ashleylevine@Ashleys-MacBook-Pro.local>
2018-09-25 22:07:26 -0400

    week 5 homework

---
+++ week5/sketch.js
@@ -0,0 +1,81 @@
+function setup() {
+  createCanvas(480, 120);
+  background(204);
+  noStroke();
+}
+
+function draw() {
+
+  if(mouseIsPressed && mouseButton== LEFT && mouseX>90 && mouseX<150 && mouseY>30 && mouseY<100 ) {
+    fill(255, 0, 0);
+  } else {
+    fill(255);
+  }
+rect(90, 30, 60, 60);
+
+if(mouseIsPressed && mouseButton== RIGHT && mouseY>30 && mouseY<90 && mouseX>330 && mouseX<390) {
+  fill(0, 255, 0);
+} else {
+  fill(255);
+}
+rect(330, 30, 60, 60);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// if(mouseY<40) {
+//   fill(255, 0, 0);
+// }else if (mouseY <80){
+//   fill(0, 0, 255);
+// } else {
+//   fill (0, 255, 0);
+// }
+//
+// ellipse(mouseX, mouseY, 10, 10);
+// }
+
+
+
+
+// if(mouseIsPressed && mouseButton== LEFT) {
+//   fill(255, 0, 0);
+// } else (mouseY<40){
+//   fill(255);
+// }
+// rect(90, 30, 60, 60);
+//
+// if() {
+//   fill(0, 255, 0);
+// } else {
+//   fill(255);
+// }
+// rect(330, 30, 60, 60);
+// }
+// }
+
+
+
+//   if(mouseX>90 && mouseX<150)
+//     fill(255,0,0);
+//   } else {
+//     fill(255);
+//   }
+// rect(90, 30, 60, 60);
+//
+//   if(mouseY>50 && mouseY<125){
+//     fill(0,255,0);
+//   } else {
+//     fill(255);
+//   }
+//   rect(330, 30, 60, 60);
+// }
