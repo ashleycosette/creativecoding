3c246d29117d68484d7c5d9a20c3e8da42810f8b
Ashley Levine <ashleylevine@Ashleys-MacBook-Pro.local>
2018-09-26 12:09:01 -0400

    week5 update

--- week5/sketch.js
+++ week5/sketch.js
@@ -6,14 +6,14 @@ function setup() {
 
 function draw() {
 
-  if(mouseIsPressed && mouseButton== LEFT && mouseX>90 && mouseX<150 && mouseY>30 && mouseY<100 ) {
+  if(mouseIsPressed && mouseButton== LEFT && mouseX>90 && mouseX<150 && mouseY>30 && mouseY<90 ) {
     fill(255, 0, 0);
   } else {
     fill(255);
   }
 rect(90, 30, 60, 60);
 
-if(mouseIsPressed && mouseButton== RIGHT && mouseY>30 && mouseY<90 && mouseX>330 && mouseX<390) {
+if(mouseIsPressed && mouseButton== LEFT && mouseY>30 && mouseY<90 && mouseX>330 && mouseX<390) {
   fill(0, 255, 0);
 } else {
   fill(255);
@@ -21,6 +21,7 @@ if(mouseIsPressed && mouseButton== RIGHT && mouseY>30 && mouseY<90 && mouseX>330
 rect(330, 30, 60, 60);
 
 }
+
 
 
 
