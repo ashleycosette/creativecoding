ca1223566926ab616085de7f4102c6eeeb6820d0
Ashley Levine <ashleylevine@Ashleys-MacBook-Pro.local>
2018-11-12 18:54:51 -0500

    week11 hw

--- week11hw/sketch.js
+++ week11hw/sketch.js
@@ -1,119 +1,46 @@
-// function Smiley(initX, initY, size, colorR, colorG, colorB) {
-//   this.x = initX;
-//   this.y = initY;
-//   this.size = size;
-//   this.R = colorR;
-//   this.G = colorG;
-//   this.B = colorB;
-//
-//   // function Rectangle(initX, initY, size, )
-//
-//   this.smile= function() {
-//     fill(this.R, this.G, this.B);
-//     rect(this.x, this.y, this.size, this.size+this.size);
-//     ellipse(this.x-this.size, this.y+this.size, 20, 70);
-//     ellipse(this.x+this.size/2, this.y+this.size/2, 20, 20);
-//     // line(this.x, this.y, this.size/2, this.size/2, 45, 135);
-//   }
-//   // this.smile2= function() {
-//   //   fill(this.R, this.G, this.B);
-//   //   rect(this.x, this.y, this.size, this.size+this.size);
-//   //   ellipse(this.x-this.size, this.y+this.size, 20, 70);
-//   //   ellipse(this.x+this.size/2, this.y+this.size/2, 20, 20);
-//   //   // line(this.x, this.y, this.size/2, this.size/2, 45, 135);
-//   // }
-// }
-//
-// var smiley1;
-// var smiley2;
-// var smiley3;
-// var smiley4;
-// var smiley5;
-// // var ellipse1;
-//
-// function setup() {
-//   createCanvas (480, 240);
-//   background(204);
-//   angleMode(DEGREES);
-//   smiley1 = new Smiley(70, 60, 80, 190, 190, 190);
-//   smiley2 = new Smiley(160, 60, 70, 190, 190, 190);
-//   smiley3 = new Smiley(240, 60, 60, 190, 190, 190);
-//   smiley4 = new Smiley(320, 60, 50, 190, 190, 190);
-//   smiley5 = new Smiley(400, 60, 40, 190, 190, 190);
-//   // ellipse1= new Ellipse(400, 60, 116, 121, 117);
-//
-// }
-//
-//
-//
-//
-//
-//
-// (25, 25, 50, 50);
-//
-// function draw() {
-//   smiley1.smile();
-//   smiley2.smile();
-//   smiley3.smile();
-//   smiley4.smile();
-//   smiley5.smile();
-//   // ellipse1.ellipse();
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-
-
-function Moai(initX, initY, size){
-
-  this.x = initX;
-  this.y = initY;
-  this.size = size;
-  this.build = function(){
-    fill(179, 179, 179);
-    rect(this.x, this.y, 75, 125);
-    fill(89, 89, 89);
-    ellipse(this.x+20, this.y+50, 20, 70);
-    ellipse(this.x+50, this.y+30, 20, 20);
-    triangle(this.x+75, this.y+75, this.x+95, this.y+75, this.x+75, this.y+30);
-    line(this.x+75, this.y+100, this.x+60, this.y+100);
-  }
+function Creature(initX, initY, size){
+  this.x = initX;
+  this.y = initY;
+  this.size = size;
+  this.buildcreature = function(){
+    fill(179, 179, 179);
+    noStroke ();
+    rect(this.x, this.y, this.size, this.size*2);
+    fill(89, 89, 89);
+    ellipse(this.x + this.size/4, this.y+ this.size, this.size/3, this.size*1.5);
+    ellipse(this.x + this.size/1.5, this.y+this.size/3, this.size/4, this.size/4);
+    triangle (this.x + this.size, this.y + this.size/3, this.x + this.size, this.y+this.size, this.x+this.size*1.2, this.y+this.size);
+    stroke (0);
+    line (this.x+this.size/2, this.y+this.size*1.5, this.x+this.size, this.y+this.size*1.5);
+  }
 }
 
 var field;
-var Moai1;
-var Moai2;
-var Moai3;
-var Moai4;
-var Moai5;
+var Creature1;
+var Creature2;
+var Creature3;
+var Creature4;
+var Creature5;
 
-function preload(){
-  field = loadImage('Moai.jpg');
+function preload() {
+field = loadImage("Moai.jpg");
 }
 
 function setup(){
-  createCanvas(700, 600);
-  Moai1 = new Moai(50, 425);
-  Moai2 = new Moai(175, 400);
-  Moai3 = new Moai(300, 375);
-  Moai4 = new Moai(425, 350);
-  Moai5 = new Moai(550, 325);
+  createCanvas(828, 549);
+  background (204);
+  Creature1 = new Creature(70, 250, 145);
+  Creature2 = new Creature(250, 250, 120);
+  Creature3 = new Creature(400, 250, 95);
+  Creature4 = new Creature(550, 250, 70);
+  Creature5 = new Creature(650, 250, 50);
 }
-//
+
 function draw(){
-  image(field, 0, 0, 1000, 700);
-  Moai1.build();
-  Moai2.build();
-  Moai3.build();
-  Moai4.build();
-  Moai5.build();
+  image(field, 0, 0);
+ Creature1.buildcreature();
+  Creature2.buildcreature();
+  Creature3.buildcreature();
+  Creature4.buildcreature();
+  Creature5.buildcreature();
 }
